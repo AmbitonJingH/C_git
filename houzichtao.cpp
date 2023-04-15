2b77a453e2e5fb2e096fbc630ad21af9e33a4c5a
AmbitionJingH <1160277072@qq.com>
2023-04-15 23:38:00 +0800

    删除重复的数

---
+++ houzichtao.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Lenovo on 2023/4/15.
+//
+#include <stdio.h>
+int taozi(int num1){
+    int num,n;
+    num = num1;
+    n=2*(num+2);
+    return n;
+}
+int main(){
+    int peach=2,day = 9;
+    while(day!=0){
+        taozi(peach);
+        peach = taozi(peach);
+        day = day - 1;
+    }
+    printf("第一天猴子摘下了%d个桃子",peach);
+    return 0;
+}
